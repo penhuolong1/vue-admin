0a295a232348ee5545c4ab57a9a15d18dfc6ee2c
penhuolong1 <1724874289@qq.com>
2020-05-25 16:35:55 +0800

    first commit

---
+++ src/pages/layout/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="app-wrapper">
+      <div class="left-wrapper"></div>
+      <div class="right-wrapper">
+        <div class="nav-wrapper"></div>
+        <div class="hmain-wrapper"></div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  props: {
+
+  },
+  components: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '~@/assets/styles/variables.scss';
+.app-wrapper {
+  &>.left-wrapper {
+    position: fixed;
+    top: 0px;
+    bottom: 0px;
+    left: 0px;
+    width: 210px;
+    background-color: $blue;
+  }
+}
+</style>
