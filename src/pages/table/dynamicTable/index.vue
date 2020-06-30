4ccee95bec7a8ffeaa7f93f988df84e7d6ba53c5
penhuolong1 <1724874289@qq.com>
2020-06-30 17:37:31 +0800

    修改文件

---
+++ src/pages/table/dynamicTable/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <aside>动态table</aside>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  props: {
+
+  },
+  components: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
